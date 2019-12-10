7540500970c2876e5a6eae292e16dc83c5141c34
wing <1157766309@qq.com>
2019-12-11 00:37:47 +0800

    2.1.7.RELEASE

--- util-arch/src/main/java/com/wing/java/util/plugin/jdbc/dialect/MySQLDialect.java
+++ util-arch/src/main/java/com/wing/java/util/plugin/jdbc/dialect/MySQLDialect.java
@@ -1,23 +1,25 @@
 package com.wing.java.util.plugin.jdbc.dialect;
-/**
- * @author badqiu
- */
-public class MySQLDialect extends Dialect{
 
-	public boolean supportsLimitOffset(){
-		return true;
-	}
-	
-    public boolean supportsLimit() {   
-        return true;   
-    }  
-    
-	public String getLimitString(String sql, int offset,String offsetPlaceholder, int limit, String limitPlaceholder) {
-        if (offset > 0) {   
-        	return sql + " limit "+offsetPlaceholder+","+limitPlaceholder; 
-        } else {   
-            return sql + " limit "+limitPlaceholder;
-        }  
-	}   
-  
+/**
+ * MySQL方言
+ */
+public class MySQLDialect extends Dialect {
+
+    public boolean supportsLimitOffset(){
+        return true;
+    }
+
+    public boolean supportsLimit() {
+        return true;
+    }
+
+    @Override
+    public String getLimitString(String sql, long offset, String offsetPlaceholder, int limit, String limitPlaceholder) {
+        if (offset > 0) {
+            return sql + " limit " + offsetPlaceholder + "," + limitPlaceholder;
+        } else {
+            return sql + " limit " + limitPlaceholder;
+        }
+    }
+
 }
